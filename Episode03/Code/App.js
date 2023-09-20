b5032059b5f482d826ff2cd77f970c97e3463db3
NamrataRahangdale <140901883+NamrataRahangdale@users.noreply.github.com>
2023-09-20 15:07:37 +0530

    Assignment03

---
+++ Episode03/Code/App.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//Create Nested header element using React.createElement(h1,h2,h3 inside div with class title)
+// const header = React.createElement("div",{class: "title"},[
+// React.createElement("h1", {},"I'm H1 tag"),
+// React.createElement("h2", {},"I'm H2 tag"),
+// React.createElement("h3", {},"I'm H3 tag"),]);
+
+//Create same with JSX
+// const header = (
+//     <div className="title">
+//         <h1>I'm H1 tag</h1>
+//         <h2>I'm H2 tag</h2>
+//         <h3>I'm H3 tag</h3>
+//     </div>
+// )
+
+
+// Functional Component of the same JSX
+// const TitleComponent = () => {
+//     return (
+//         <div className="title">
+//             <h1>I'm H1 tag</h1>
+//             <h2>I'm H2 tag</h2>
+//             <h3>I'm H3 tag</h3>
+//         </div>
+//     );
+// };
+
+//Composition of Components
+// const HeadingCompo = () => (
+//     <div id="container">
+//         <h1 className="heading">Fuctional Component </h1>
+//         <TitleComponent />
+//         {TitleComponent()}
+//         {<TitleComponent></TitleComponent>}
+//     </div>
+//);
+
+//Create Header component from scratch using fuctional component in Jsx
+const Header = () => (
+    <div id="container">
+        <img className='logo' src='https://img.icons8.com/?size=2x&id=bzf0DqjXFHIW&format=png' />
+
+        <div className='search'>
+            <input style={{width:150, height:40}} placeholder='Search' />
+        </div>
+
+        <img className='user' src='https://static.vecteezy.com/system/resources/thumbnails/008/442/086/small/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg' />
+
+    </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(title);
+//root.render(<HeadingCompo />);
+root.render(<Header />);
